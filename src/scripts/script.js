23381d08e35f30a77af3e326fcbcd4dd078932ca
EBolle <ernstbolle@gmail.com>
2023-05-02 09:47:25 +0200

    To make this work with POS you have to completely rethink the regular expressions, which is fine, focus on POS for now since that's the basis to better understand your writing patterns

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,45 +3,54 @@
 import { regexPatterns } from "./modules/regex-patterns.js";
 import { getPosList, lyAdverbs, nlpPatterns } from "./modules/nlp-patterns.js";
 
+const outputContainer = document.querySelector("#output-container");
 const textArea = document.querySelector("#text-area");
-const textContainer = document.querySelector("#text-container");
 
-const textClasses = ["py-2"];
-const posClasses = ["text-sm", "text-orange-500"];
+const textPosDivClasses = ["flex", "flex-col", "space-y-1"];
+const posClasses = ["text-medium", "text-orange-500"];
 
 // Initialize with 0 so always get the correct slice after a keyup event
 const cursorIdxArray = [0];
 
 textArea.addEventListener("keyup", (event) => {
   if (event.code === "Period" || event.key === "?" || event.key === "!") {
-    let newParagraph = document.createElement("p");
-    newParagraph.classList.add(...textClasses);
-    textContainer.append(newParagraph);
-
     cursorIdxArray.push(textArea.selectionStart);
-
     let text = textArea.value.slice(cursorIdxArray.at(-2));
 
     getPosList(text).then((data) => {
-      console.log(data);
-      let posParagraph = document.createElement("p");
-      posParagraph.classList.add(...posClasses);
-      posParagraph.innerHTML = data.pos.join(" ");
-      newParagraph.append(posParagraph);
-    });
+      const textPos = data.text.map(function (value, idx) {
+        return [value, data.pos[idx]];
+      });
 
-    regexPatterns.forEach(
-      (regex) =>
-        (text = text.replaceAll(
-          regex.pattern,
-          `<span class=${regex.name} title=${regex.description}>$1</span>`
-        ))
-    );
+      textPos.forEach((element) => {
+        let textPosDiv = document.createElement("div");
+        textPosDiv.classList.add(...textPosDivClasses);
 
-    newParagraph.innerHTML = text;
+        let textSpan = document.createElement("span");
+        let posSpan = document.createElement("span");
+        posSpan.classList.add(...posClasses);
 
-    lyAdverbs(text).then((data) => {
-      newParagraph.innerHTML = data;
+        textSpan.innerHTML = element[0];
+        posSpan.innerHTML = element[1];
+        textPosDiv.append(textSpan);
+        textPosDiv.append(posSpan);
+
+        outputContainer.append(textPosDiv);
+      });
     });
   }
 });
+
+// regexPatterns.forEach(
+//   (regex) =>
+//     (text = text.replaceAll(
+//       regex.pattern,
+//       `<span class=${regex.name} title=${regex.description}>$1</span>`
+//     ))
+// );
+
+// newParagraph.innerHTML = text;
+
+// lyAdverbs(text).then((data) => {
+//   newParagraph.innerHTML = data;
+// });
